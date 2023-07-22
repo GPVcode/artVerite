1fce863902a788ad2f416600fce9145d5e31f8e1
GPV <gvill005@gmail.com>
2023-07-21 17:20:07 -0700

    Selected Auto complete option handler

--- client/script.js
+++ client/script.js
@@ -1,21 +1,28 @@
-
+// autocomplete handler
+let autocomplete;
 function initialize() {
     const input = document.getElementById('autocomplete');
     const options = {
-        componentRestrictions: { country: ["us"]},
+        componentRestrictions: { 'country': ["us"]},
         fields: ["name"],
         types: ["school"]
-    }
-    const autocomplete = new google.maps.places.Autocomplete(input, options);
+    };
+    autocomplete = new google.maps.places.Autocomplete(input, options);
 
-      google.maps.event.addListener(autocomplete, 'place_changed', function () {
-          var place = autocomplete.getPlace();
+    autocomplete.addListener('place_changed', onPlaceChanged);
 
-          if(!place.geometry){}
-          document.getElementById('city2').value = place.name;
-          document.getElementById('cityLat').value = place.geometry.location.lat();
-          document.getElementById('cityLng').value = place.geometry.location.lng();
-      });
-  }
-  window.addEventListener('load', initialize);
+}
+
+// handles what happens when autocomplete option is clicked
+function onPlaceChanged(){
+    // get info about selected school
+    const place = autocomplete.getPlace();
+    console.log("place", place.name)
+
+        // display details about the valid place
+        document.getElementById('details').innerHTML = place.name;
+
+}
+
+window.addEventListener('load', initialize);
 
